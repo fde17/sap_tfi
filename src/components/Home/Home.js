2635cf213caf1b8c3bf7dedabe9966ba8137a91f
Gastón Donati <gdonati88@gmail.com>
2020-12-05 10:42:37 -0300

    base de Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import { Layout } from 'antd';
+
+const { Footer,} = Layout;
 
 class Home extends Component {
+
     render() {
 
-        return (
-        <div> Hola gato</div>
+        return ( 
+            <Layout style = {{ minHeight: '75vh' }}>
+                <Layout className = "site-layout" ></Layout>
+                <div class="container">
+                  <main class="content">
+                   <login class="login">
+                       Home
+                   </login>
+                  </main>
+                </div>  
+                <Footer class="footer" style={{ color: 'white', height: 100, backgroundColor: 'black', textAlign: 'center' }}>Go Beauty ©2020</Footer>            
+            </Layout>
         );
     }
 }
+
 
 export default Home;
